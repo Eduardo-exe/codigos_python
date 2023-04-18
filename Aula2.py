b256c82c7a816dcc4b8d5ccda4108301117ae716
Eduardo Oliveira <eduardo.oliveira.exe@gmail.com>
2023-04-17 22:11:50 -0300

    Criado aula 2

---
+++ Aula2.py
@@ -0,0 +1,13 @@
+nome = input("informe seu nome: ")
+idade = input("informe sua idade: ")
+#isso é um comentário de uma linha
+"""
+isso é um comentário de varias linhas
+
+"""
+#Primeira froma de concatenar 
+
+print("Olá ",nome," sua idade é ",idade)
+print("Olá {} sua idade é {}".format(nome,idade))
+print(f"Olá {nome} sua idade é {idade}")
+
